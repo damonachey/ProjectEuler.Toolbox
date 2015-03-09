13233ad6a64955a3831abb353b6f28dab58419f2
Damon Achey <Damon@Achey.Net>
2015-03-08 20:16:42 -0600

    Cleaned up code from previous delete

--- Toolbox/Polynomial.cs
+++ Toolbox/Polynomial.cs
@@ -42,48 +42,5 @@ namespace ProjectEuler.Toolbox
                 x += dx;
             }
         }
-
-        /// <summary>
-        /// Generate the Lagrange coefficients given a set of points
-        ///
-        /// http://en.wikipedia.org/wiki/Lagrange_polynomial
-        ///
-        /// TODO: Finish Lagrange simplification
-        /// </summary>
-        /// <param name="points">List of points to compute coefficients from.</param>
-        /// <returns></returns>
-        public static IEnumerable<Point2DBigRational> LagrangeCoefficients(IList<Point2DBigRational> points)
-        {
-            var l = Enumerable
-                .Repeat("1", points.Count)
-                .ToList();
-
-            for (var j = 0; j < l.Count; j++)
-            {
-                for (var k = 0; k < points.Count; k++)
-                {
-                    if (k != j)
-                    {
-                        // l[j] *= (x - points[k].X) / (points[j].X - points[k].X);
-                        l[j] += " * (x - " + points[k].X + ") / " + (points[j].X - points[k].X);
-                    }
-                }
-            }
-
-            var y = "0";
-
-            for (int i = 0; i < points.Count; i++)
-            {
-                y += " + " + points[i].Y + " * " + l[i];
-            }
-
-            System.Diagnostics.Debug.WriteLine(y);
-
-            y = Expression.Simplify(y);
-
-            System.Diagnostics.Debug.WriteLine(y);
-
-            throw new NotImplementedException();
-        }
     }
 }
