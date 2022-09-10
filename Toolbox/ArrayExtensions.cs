ac7d25a1b1c7dddaca9e4d0c1e8470d9a5477301
Damon Achey <damon@achey.net>
2022-09-09 23:07:23 -0600

    Add new higher performance array copy

---
+++ Toolbox/ArrayExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectEuler.Toolbox;
+
+public static class ArrayExtensions
+{
+    public static T[] ToArray<T>(this T[] arr)
+    {
+        var result = new T[arr.Length];
+        arr.AsSpan().CopyTo(result);
+        return result;
+    }
+}
---
+++ ToolboxTests/ArrayExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using ProjectEuler.Toolbox;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Xunit;
+
+namespace ProjectEuler.ToolboxTests;
+
+public class ArrayExtensionsTests
+{
+    [Fact]
+    public void ToArrayTest()
+    {
+        var expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        var actual = ArrayExtensions.ToArray(expected);
+
+        Assert.NotSame(expected, actual);
+        Assert.Equal(expected.Length, actual.Length);
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void ToArrayPerformanceTest()
+    {
+        var arr = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+        var sw1 = Stopwatch.StartNew();
+        _ = ArrayExtensions.ToArray(arr);
+        sw1.Stop();
+
+        var sw2 = Stopwatch.StartNew();
+        _ = Enumerable.ToArray(arr);
+        sw2.Stop();
+
+        Assert.True(sw1.Elapsed < sw2.Elapsed);
+    }
+}
